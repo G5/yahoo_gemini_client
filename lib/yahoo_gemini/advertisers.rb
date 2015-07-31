37e5f97bcacb796df7e3317929ee6823049ac504
Ramon Tayag <ramon.tayag@gmail.com>
2015-07-31 11:54:51 +0800

    Add Advertisers#find

--- lib/yahoo_gemini/advertisers.rb
+++ lib/yahoo_gemini/advertisers.rb
@@ -17,5 +17,15 @@ module YahooGemini
       end
     end
 
+    def find(ids)
+      uri = URI.parse('https://api.admanager.yahoo.com/v1/rest/advertiser/')
+      uri.query = ids.map { |id| "id=#{id}" }.join("&")
+      response = HTTParty.get(uri.to_s, headers: api_request_headers)
+      parsed = JSON.parse(response.body)
+      parsed["response"].map do |advertiser_hash|
+        Advertiser.new(advertiser_hash)
+      end
+    end
+
   end
 end
---
+++ spec/yahoo_gemini/advertisers_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+module YahooGemini
+  describe Advertisers do
+    let(:client) do
+      client = Client.new(
+        consumer_key: consumer_key,
+        consumer_secret: consumer_secret,
+      )
+      client.get_token(authorization_code)
+      client
+    end
+    let(:consumer_key) { ENV["YAHOO_GEMINI_TEST_CONSUMER_KEY"] }
+    let(:consumer_secret) { ENV["YAHOO_GEMINI_TEST_CONSUMER_SECRET"] }
+    let(:authorization_code) { ENV["YAHOO_GEMINI_TEST_AUTHORIZATION_CODE"] }
+
+    describe "#find", vcr: { record: :once } do
+      let(:advertisers_to_find) { client.advertisers.to_a[0..1] }
+
+      it "looks for advertisers matching given id/s" do
+        advertisers = described_class.new(client: client).
+          find(advertisers_to_find.map(&:id))
+
+        expect(advertisers.count).to eq 2
+        expect(advertisers[0].advertiser_name).
+          to eq advertisers_to_find[0].advertiser_name
+        expect(advertisers[1].advertiser_name).
+          to eq advertisers_to_find[1].advertiser_name
+      end
+    end
+  end
+end
