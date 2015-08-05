856c1c970f3181dfb658c58d392ab07b4f992e7c
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2015-08-05 13:42:24 +0800

    Added CustomReportRequest & CustomReportResponse
    [#99871316]

---
+++ lib/yahoo_gemini_client/requests/custom_report_job_request.rb
@@ -0,0 +1,16 @@
+module YahooGeminiClient
+  class CustomReportJobRequest < BaseRequest
+    REQUEST_URI = "https://api.admanager.yahoo.com/v1/rest/reports/custom"
+
+    attribute :request_body
+
+    def execute
+      http_authorization_header = {"Authorization" => "Bearer #{access_token}"}
+      response = HTTParty.post(REQUEST_URI,
+        headers: http_request_header.merge(http_authorization_header),
+        body: request_body.to_json,
+      )
+      CustomReportJobResponse.new(response.with_indifferent_access)
+    end
+  end
+end
