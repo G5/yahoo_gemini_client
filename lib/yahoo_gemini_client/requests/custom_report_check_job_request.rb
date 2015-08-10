481768c0183c283995ac26328cfcf51cd343583b
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2015-08-10 09:25:59 +0800

    Refactor custom report request classes

--- lib/yahoo_gemini_client/requests/custom_report_check_job_request.rb
+++ lib/yahoo_gemini_client/requests/custom_report_check_job_request.rb
@@ -1,16 +1,12 @@
 module YahooGeminiClient
-  class CustomReportCheckJobRequest < BaseRequest
+  class CustomReportCheckJobRequest < CustomReportBaseRequest
     REQUEST_URI = "https://api.admanager.yahoo.com/v1/rest/reports/custom/"
 
-    attribute :request_body, String
     attribute :advertiser_id, String
     attribute :job_id, String
 
     def execute
-      http_authorization_header = {"Authorization" => "Bearer #{access_token}"}
-      response = HTTParty.get(build_request_uri,
-        headers: http_request_header.merge(http_authorization_header),
-      )
+      response = get(request_uri: build_request_uri)
       CustomReportResponse.new(response.with_indifferent_access)
     end
 
